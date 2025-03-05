2b03ef44364ad4615ff51e7681b0b9e27ce98815
Eaglethatlives <jessuxdesigner@gmail.com>
2025-03-05 23:27:28 +0000

    statistics_and_trends.py

--- statistics_and_trends.py
+++ statistics_and_trends.py
@@ -7,7 +7,6 @@ You should NOT change any function, file or variable names,
 Make use of the functions presented in the lectures
 and ensure your code is PEP-8 compliant, including docstrings.
 """
-
 from corner import corner
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,52 +15,33 @@ import scipy.stats as ss
 import seaborn as sns
 
 def plot_relational_plot(df):
-   """
-   This Section Creates a 2x2 subplot with scatter plots.
-   """
-    
    fig, ax = plt.subplots(2, 2, figsize=(12, 10))  
    ax = ax.flatten()  
-    
    plot_pairs = [('H', 'S'), ('H', 'V'), ('S', 'V')]  
    titles = ['MAC Brands Hue (H) vs. Saturation (S)', 'Fenty Brands Hue (H) vs. Value (V)', 'Revlon Brands Saturation (S) vs. Hue (H)']
    colors = ['purple', 'pink', 'yellow']
-
    for i, ((x_col, y_col), title, color) in enumerate(zip(plot_pairs, titles, colors)):
         sns.scatterplot(x=df[x_col], y=df[y_col], alpha=0.5, color=color, ax=ax[i])
         ax[i].set_title(title)
         ax[i].set_xlabel(x_col)
         ax[i].set_ylabel(y_col)
-
    fig.delaxes(ax[3])
    plt.tight_layout()
    plt.savefig('relational_plot.png')
    plt.show()
-
-
-    
-
+   
 def plot_categorical_plot(df):
-    """
-    This Section creates a count plot, bar plot, and pie chart for categorical analysis.
-   """
     fig, ax = plt.subplots(figsize=(10, 12), dpi=144)
     sns.countplot(y='brand', data=df,hue='group',palette='viridis', ax=ax)
-
     ax.set_title("Number of MakeUp Products by Brand", fontsize=16)
     ax.set_xlabel("Count", fontsize=12)
     ax.set_ylabel("Brand", fontsize=12)
-
     plt.tight_layout()
     plt.show()
-    
-    """
-     A bar plot
-     """
      
     fig, ax = plt.subplots(figsize=(12, 8))
-    sns.barplot(x='brand', y='L',hue='group', data=df, errorbar=None, palette=["pink", "yellow", "blue", "purple", "black", "brown", "red", "orange"])
-
+    sns.barplot(x='brand', y='L',hue='group', data=df, errorbar=None, palette=[
+    "pink", "yellow", "blue", "purple", "black", "brown", "red", "orange"])
     plt.title("A Bar Plot for Average Lightness (L) for different MakeUp Brands", fontsize=16)
     plt.xlabel("Brand", fontsize=12)
     plt.ylabel("Average Lightness (L)", fontsize=12)
@@ -70,54 +50,33 @@ def plot_categorical_plot(df):
     plt.tight_layout()
     plt.savefig("categorical_plot.png")  
     plt.show()
-     
-     
-    """
-   A pie chart .
-   """
-     
     brands = [30, 25, 20, 15, 10]  
     labels = ['MAC', 'Fenty', 'bareminerals', 'Revlon', 'Dior']  # Category labels
-
     plt.figure(dpi=144)
     plt.pie(brands, labels=labels, autopct='%1.1f%%', startangle=140, colors=
     ['#ff0000','#66b4ff', '#99ff99', '#ffcc99', '#445bbb'])
     plt.title('Pie chart of Largest Make-Up brands ')
     plt.axis('equal')
     plt.show()
-
     plt.savefig('categorical_plot.png')
     return
 
-
 def plot_statistical_plot(df):
-    
-    """
-   This section creates a 2x2 subplot grid with histograms for statistical analysis.
-   """
-    
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 10))
     fig.suptitle("Subplots for MakeUp Products ", fontsize=16)
-
     
     ax1.hist(df['H'], bins=20, color='blue', edgecolor='black')
     ax1.set_title("Distribution of Hue in MAC Products ")
     ax1.set_xlabel("Hue (H)")
     ax1.set_ylabel("Frequency")
-
-
     ax2.hist(df['S'], bins=20, color='green', edgecolor='black')
     ax2.set_title("Distribution of Saturation in Maybelline products")
     ax2.set_xlabel("Saturation (S)")
     ax2.set_ylabel("Frequency")
-
-    
     ax3.hist(df['S'], bins=20, color='purple', edgecolor='black')
     ax3.set_title("Distribution of Saturation in Fenty products")
     ax3.set_xlabel("Saturation (S)")
     ax3.set_ylabel("Frequency")
-
-    
     ax4.hist(df['L'], bins=20, color='red', edgecolor='black')
     ax4.set_title("Distribution of Lightness in baremineral Products")
     ax4.set_xlabel("Lightness (L)")
@@ -126,32 +85,23 @@ def plot_statistical_plot(df):
     plt.tight_layout(rect=[0, 0, 1, 0.96])
     plt.savefig('statistical_plot.png')
     plt.show()
-    
     return
 
-
 def statistical_analysis(df, col: str):
-    
     mean = df[col].mean()
     stddev = df[col].std()
     skew = df[col].skew()
     excess_kurtosis = df[col].kurt()
     return mean, stddev, skew, excess_kurtosis
 
-
 def preprocessing(df):
     df.describe()
     print("Index of the DataFrame (brand):")
     print(df.index)  
-
-
     print("\nFirst few rows of the DataFrame:")
     print(df.head())  
-
     df = df.dropna()
-   
     return df
-
 
 def writing(moments, col):
     
@@ -162,7 +112,6 @@ def writing(moments, col):
           f'Skewness = {skew:.2f}, and '
           f'Excess Kurtosis = {excess_kurtosis:.2f}.')
    
-    
     if skew > 2:
        skew = "right-skewed"
     elif skew < -2:
@@ -170,20 +119,16 @@ def writing(moments, col):
     else:
        skew = "not skewed"
 
-   
     if excess_kurtosis > 1:
        excess_kurtosis= "leptokurtic"
     elif excess_kurtosis < -1:
       excess_kurtosis = "platykurtic"
     else:
        excess_kurtosis = "mesokurtic"
-       
-    print('The data was right/left/not skewed and platy/meso/leptokurtic.')
+       print('The data was right/left/not skewed and platy/meso/leptokurtic.')
     return
 
-
 def main():
-    
     df = pd.read_csv('data.csv')
     df = preprocessing(df)
     col = 'H'
@@ -192,10 +137,7 @@ def main():
     plot_categorical_plot(df)
     moments = statistical_analysis(df, col)
     writing(moments, col)
-    
     return
-
 
 if __name__ == '__main__':
     main()
-
